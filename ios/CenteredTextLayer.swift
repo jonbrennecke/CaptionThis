6e025cbfd6ebac425c0c8c7626f24156db8e0374
Jon Brennecke <jpbrennecke@gmail.com>
2019-01-03 01:49:44 -0800

    vertically centered text in exported video

--- ios/CenteredTextLayer.swift
+++ ios/CenteredTextLayer.swift
@@ -1,28 +1,9 @@
 import Foundation
 
-// https://github.com/cemolcay/CenterTextLayer/blob/master/Source/CenterTextLayer.swift
+// https://stackoverflow.com/questions/4765461/vertically-align-text-in-a-catextlayer/41518502#41518502
 public class CenteredTextLayer: CATextLayer {
-  
-  public override init() {
-    super.init()
-  }
-  
-  public override init(layer: Any) {
-    super.init(layer: layer)
-  }
-  
-  public required init(coder aDecoder: NSCoder) {
-    super.init(layer: aDecoder)
-  }
-  
   public override func draw(in ctx: CGContext) {
-    #if os(iOS) || os(tvOS)
-    let multiplier = CGFloat(1)
-    #elseif os(OSX)
-    let multiplier = CGFloat(-1)
-    #endif
-    let yDiff = (bounds.size.height - ((string as? NSAttributedString)?.size().height ?? fontSize)) / 2 * multiplier
-    
+    let yDiff = (bounds.size.height - fontSize) / 2 - fontSize / 10
     ctx.saveGState()
     ctx.translateBy(x: 0.0, y: yDiff)
     super.draw(in: ctx)
