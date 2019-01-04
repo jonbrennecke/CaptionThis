cc3c324b85e67f773d44245fc40e456ad75cb255
Jon Brennecke <jpbrennecke@gmail.com>
2019-01-03 16:13:52 -0800

    added linear gradient to SwitchCameraButton

--- src/components/switch-camera-button/SwitchCameraButton.js
+++ src/components/switch-camera-button/SwitchCameraButton.js
@@ -1,8 +1,10 @@
 // @flow
 import React from 'react';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, MaskedViewIOS } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 import SwitchCameraIcon from './SwitchCameraIcon';
+import { UI_COLORS } from '../../constants';
 
 import type { Style } from '../../types/react';
 
@@ -16,6 +18,13 @@ const styles = {
   flex: {
     flex: 1,
   },
+  absoluteFill: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
 };
 
 export default function SwitchCameraButton({
@@ -27,7 +36,19 @@ export default function SwitchCameraButton({
       onPress={onRequestSwitchCamera}
       style={[styles.container, style]}
     >
-      <SwitchCameraIcon style={styles.flex} />
+      <MaskedViewIOS
+        style={styles.absoluteFill}
+        maskElement={<SwitchCameraIcon style={styles.flex} />}
+      >
+        <LinearGradient
+          pointerEvents="none"
+          useAngle
+          angle={-45}
+          angleCenter={{ x: 0.5, y: 0.5 }}
+          colors={[UI_COLORS.LIGHT_GREEN, UI_COLORS.MEDIUM_GREEN]}
+          style={styles.absoluteFill}
+        />
+      </MaskedViewIOS>
     </TouchableOpacity>
   );
 }
