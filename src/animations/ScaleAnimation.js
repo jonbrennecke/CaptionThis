5119a33e28f5dfbe5d419f95e7cb809daac02659
Jon Brennecke <jpbrennecke@gmail.com>
2019-02-19 15:09:31 -0800

    added scale-in animation to edit screen

---
+++ src/animations/ScaleAnimation.js
@@ -0,0 +1,43 @@
+// @flow
+import { Animated, Easing } from 'react-native';
+
+import type { Style } from '../types/react';
+import type { Animation } from './types';
+
+export default class ScaleAnimation implements Animation {
+  value: Animated.Value;
+  delay: number;
+
+  constructor({ start, delay = 0 }: { start: 'in' | 'out', delay?: number }) {
+    this.value = new Animated.Value(start === 'in' ? 1 : 0);
+    this.delay = delay;
+  }
+
+  animateIn(completionHandler?: () => void) {
+    Animated.timing(this.value, {
+      toValue: 1,
+      duration: 300,
+      easing: Easing.quad,
+      useNativeDriver: true,
+    }).start(completionHandler);
+  }
+
+  animateOut(completionHandler?: () => void) {
+    Animated.timing(this.value, {
+      toValue: 0,
+      duration: 300,
+      easing: Easing.quad,
+      useNativeDriver: true,
+    }).start(completionHandler);
+  }
+
+  getAnimatedStyle(): Style {
+    return {
+      opacity: this.value,
+      transform: [{ scale: this.value.interpolate({
+        inputRange: [0, 1],
+        outputRange: [0.8, 1],
+      }) }],
+    };
+  }
+}
---
+++ src/components/animations/ScaleAnimatedView.js
@@ -0,0 +1,49 @@
+// @flow
+import React, { Component } from 'react';
+import { Animated } from 'react-native';
+
+import ScaleAnimation from '../../animations/ScaleAnimation';
+
+import type { Style, Children } from '../../types/react';
+
+type Props = {
+  style?: ?Style,
+  isVisible: boolean,
+  children: ?Children,
+  onFadeInDidComplete?: () => void,
+  onFadeOutDidComplete?: () => void,
+};
+
+const styles = {
+  fade: (anim: ScaleAnimation) => anim.getAnimatedStyle(),
+};
+
+export default class ScaleAnimatedView extends Component<Props> {
+  anim: ScaleAnimation;
+
+  constructor(props: Props) {
+    super(props);
+    this.anim = new ScaleAnimation({
+      start: this.props.isVisible ? 'in' : 'out',
+    });
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    if (this.props.isVisible && !prevProps.isVisible) {
+      this.anim.animateIn(this.props.onFadeInDidComplete);
+    } else if (!this.props.isVisible && prevProps.isVisible) {
+      this.anim.animateOut(this.props.onFadeOutDidComplete);
+    }
+  }
+
+  render() {
+    return (
+      <Animated.View
+        style={[this.props.style, styles.fade(this.anim)]}
+        pointerEvents={this.props.isVisible ? 'auto' : 'none'}
+      >
+        {this.props.children}
+      </Animated.View>
+    );
+  }
+}
