1e431ba17f06d3c996e4b5f1eedddc2b87e7adcc
Jon Brennecke <jpbrennecke@gmail.com>
2019-02-19 17:14:07 -0800

    added custom edit screen icons

--- src/components/icons/ExportIcon.js
+++ src/components/icons/ExportIcon.js
@@ -15,10 +15,10 @@ const styles = {
   },
 };
 
-export default function ColorPaletteIcon({ style }: Props) {
+export default function ExportIcon({ style }: Props) {
   return (
     <View style={[styles.container, style]}>
-      <Image source={{ uri: 'ColorPaletteIcon' }} style={styles.flex} />
+      <Image source={{ uri: 'ExportIcon' }} style={styles.flex} />
     </View>
   );
 }
