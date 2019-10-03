84bf1a2ac28af188261beff599d933baea0bb4ba
Jon Brennecke <jpbrennecke@gmail.com>
2019-10-03 13:31:43 -0700

    format

--- ios/SpeechBridgeModule.m
+++ ios/SpeechBridgeModule.m
@@ -205,12 +205,12 @@ RCT_EXPORT_METHOD(setLocale
   callback(@[ [NSNull null], @(success) ]);
 }
 
-RCT_EXPORT_METHOD(hasSpeechPermissions: (RCTResponseSenderBlock)callback) {
+RCT_EXPORT_METHOD(hasSpeechPermissions : (RCTResponseSenderBlock)callback) {
   BOOL isAuthorized = [AppDelegate.sharedSpeechManager isAuthorized];
   callback(@[ [NSNull null], @(isAuthorized) ]);
 }
 
-RCT_EXPORT_METHOD(requestSpeechPermissions: (RCTResponseSenderBlock)callback) {
+RCT_EXPORT_METHOD(requestSpeechPermissions : (RCTResponseSenderBlock)callback) {
   [AppDelegate.sharedSpeechManager authorize:^(BOOL isAuthorized) {
     callback(@[ [NSNull null], @(isAuthorized) ]);
   }];
--- src/redux/onboarding/actions.js
+++ src/redux/onboarding/actions.js
@@ -1,20 +1,34 @@
 // @flow
 import Promise from 'bluebird';
 import { authorizeMediaLibrary } from '@jonbrennecke/react-native-media';
-import { requestCameraPermissions, hasCameraPermissions } from '@jonbrennecke/react-native-camera';
+import {
+  requestCameraPermissions,
+  hasCameraPermissions,
+} from '@jonbrennecke/react-native-camera';
 
-import { requestSpeechPermissions, hasSpeechPermissions } from '../../utils/SpeechManager';
+import {
+  requestSpeechPermissions,
+  hasSpeechPermissions,
+} from '../../utils/SpeechManager';
 
 export const arePermissionsGranted = async (): Promise<boolean> => {
   const areCameraPermissionsGranted = await hasCameraPermissions();
   const areSpeechPermissionsGranted = await hasSpeechPermissions();
   const areMediaPermissionsGranted = await authorizeMediaLibrary(); // TODO
-  return areCameraPermissionsGranted && areSpeechPermissionsGranted && areMediaPermissionsGranted;
+  return (
+    areCameraPermissionsGranted &&
+    areSpeechPermissionsGranted &&
+    areMediaPermissionsGranted
+  );
 };
 
 export const requestAppPermissions = async (): Promise<boolean> => {
   const areCameraPermissionsGranted = await requestCameraPermissions();
   const areSpeechPermissionsGranted = await requestSpeechPermissions();
   const areMediaPermissionsGranted = await authorizeMediaLibrary();
-  return areCameraPermissionsGranted && areSpeechPermissionsGranted && areMediaPermissionsGranted;
+  return (
+    areCameraPermissionsGranted &&
+    areSpeechPermissionsGranted &&
+    areMediaPermissionsGranted
+  );
 };
