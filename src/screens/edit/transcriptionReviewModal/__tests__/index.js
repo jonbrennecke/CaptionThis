76bd54aef1154cf54c0140c7ce30f5712aeb8971
Jon Brennecke <jpbrennecke@gmail.com>
2019-10-07 11:06:30 -0700

    improving tests

--- src/screens/edit/transcriptionReviewModal/__tests__/index.js
+++ src/screens/edit/transcriptionReviewModal/__tests__/index.js
@@ -1,7 +1,12 @@
 // @flow
-import type { SpeechTranscriptionSegment } from '../../../../types';
+import last from 'lodash/last';
 
-import { transformSegmentsByTextDiff, interpolateSegments } from '../transcriptionReviewUtils';
+import {
+  transformSegmentsByTextDiff,
+  interpolateSegments,
+} from '../transcriptionReviewUtils';
+
+import type { SpeechTranscriptionSegment } from '../../../../types';
 
 const segments: Array<SpeechTranscriptionSegment> = interpolateSegments([
   {
@@ -31,12 +36,40 @@ const segments: Array<SpeechTranscriptionSegment> = interpolateSegments([
     confidence: 1,
     alternativeSubstrings: [],
     timestamp: 0.75,
-  }
+  },
 ]);
 
 describe('transformSegmentsByTextDiff', () => {
-  it('transforms segments when a single word/segment is  hanged', async () => {
-    const transformedSegments = transformSegmentsByTextDiff('the quick blue fox', segments);
+  it('produces the expected result when a single word/segment is changed', () => {
+    const transformedSegments = transformSegmentsByTextDiff(
+      'the quick blue fox',
+      segments
+    );
     expect(transformedSegments[4].substring).toBe('blue');
+  });
+
+  it('produces the expected result when multiple words/segments are changed', () => {
+    const transformedSegments = transformSegmentsByTextDiff(
+      'the slow blue fox',
+      segments
+    );
+    expect(transformedSegments[2].substring).toBe('slow');
+    expect(transformedSegments[4].substring).toBe('blue');
+  });
+
+  it('produces the expected result when the first word/segment is changed', () => {
+    const transformedSegments = transformSegmentsByTextDiff(
+      'a quick brown fox',
+      segments
+    );
+    expect(transformedSegments[0].substring).toBe('a');
+  });
+
+  it('produces the expected result when the last word/segment is changed', () => {
+    const transformedSegments = transformSegmentsByTextDiff(
+      'the quick brown cat',
+      segments
+    );
+    expect(last(transformedSegments).substring).toBe('cat');
   });
 });
