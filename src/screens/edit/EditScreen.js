cf07e4b9427a11172ffa482530e393d205b559d9
Jon Brennecke <jpbrennecke@gmail.com>
2019-10-10 12:34:38 -0700

    removed comment

--- src/screens/edit/EditScreen.js
+++ src/screens/edit/EditScreen.js
@@ -358,7 +358,6 @@ export default class EditScreen extends PureComponent<Props, State> {
   }
 
   render() {
-    console.log(this.state.orientation);
     return (
       <View style={styles.container}>
         {!this.props.isSpeechTranscriptionFinal && (
